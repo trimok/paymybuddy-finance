8804c421b0776dcbbfd573e29251ba3c948b1344
trimok <trimok@mail.fr>
2022-11-30 15:32:28 +0100

    JavaDoc test

--- src/main/java/com/paymybuddy/finance/PayMyBuddyFinanceApplication.java
+++ src/main/java/com/paymybuddy/finance/PayMyBuddyFinanceApplication.java
@@ -3,11 +3,16 @@ package com.paymybuddy.finance;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 
+ * @author trimok
+ *
+ */
 @SpringBootApplication
 public class PayMyBuddyFinanceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PayMyBuddyFinanceApplication.class, args);
-	}
+    public static void main(String[] args) {
+	SpringApplication.run(PayMyBuddyFinanceApplication.class, args);
+    }
 
 }
