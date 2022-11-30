de938c558ed8899742a8b48b2c7994f3466b045a
trimok <trimok@mail.fr>
2022-11-30 15:29:56 +0100

    Refactoring main application name

--- src/main/java/com/paymybuddy/finance/PayMyBuddyFinanceApplication.java
+++ src/main/java/com/paymybuddy/finance/PayMyBuddyFinanceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PaymybuddyFinanceApplication {
+public class PayMyBuddyFinanceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PaymybuddyFinanceApplication.class, args);
+		SpringApplication.run(PayMyBuddyFinanceApplication.class, args);
 	}
 
 }
